b467aba93e5c87c03e517861b8b8a5ca8afb13d2
Carloseira <carlosaf@outlook.com.br>
2022-01-15 21:18:47 -0300

    Project fixed

--- backend/src/main/java/devsuperior/dsmovie/repositories/ScoreRepository.java
+++ backend/src/main/java/devsuperior/dsmovie/repositories/ScoreRepository.java
@@ -3,8 +3,8 @@ package devsuperior.dsmovie.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import devsuperior.dsmovie.entities.Score;
-import devsuperior.dsmovie.entities.ScorePK;
+import devsuperior.dsmovie.entities.ScorePk;
 
-public interface ScoreRepository extends JpaRepository<Score, ScorePK>{
+public interface ScoreRepository extends JpaRepository<Score, ScorePk>{
 
 }
